4fcf3b684c499552a5a3c0698399fbc3aded0fb3
Rustam <toymastov@gmail.com>
2023-02-09 20:04:36 +0400

    customTableViewCell

--- tableViewApp/CustomTableViewCell.swift
+++ tableViewApp/CustomTableViewCell.swift
@@ -10,5 +10,14 @@ import UIKit
 class CustomTableViewCell: UITableViewCell {
 
  
-
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
 }
